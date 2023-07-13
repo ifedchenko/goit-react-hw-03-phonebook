bfe3d55c7b2b9fbcce9bfe38114237b38e002c16
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-07-13 15:48:52 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,7 +26,7 @@ export class App extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevState.contact !== this.state.contacts) {
+    if (prevState.contacts !== this.state.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
